aa16c486389653bffdba2ddc62fc3ade041d97f9
karoly vilenczi <karoly.vilenczi@gmail.com>
2024-10-30 07:00:26 -0400

    added emot logic call to server

--- test_flask.sh
+++ test_flask.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -X POST http://localhost:5000/emotionDetector -H "Content-Type: application/json"  -d  "text=I love my life."
+# curl -X POST http://localhost:5000/emotionDetector -H "Content-Type: application/json"  -d  "text=I love my life."
+
+curl -X POST http://127.0.0.1:5000/emotionDetector -H "Content-Type: application/json" -d '{"text": "I love my life"}'
