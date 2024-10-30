668e5e5c815aaa0adb25620402ddebcfb092b5c9
karoly vilenczi <karoly.vilenczi@gmail.com>
2024-10-30 05:54:38 -0400

    ref to keep 1 func

--- emotion_detection.py
+++ emotion_detection.py
@@ -3,13 +3,6 @@ from pprint import pp
 import requests
 import json
 
-def find_dominant_emotion(resp_emotions:dict) -> dict:
-    dominant_emotion = max(resp_emotions, key = resp_emotions.get )
-    print(f"{dominant_emotion=}")
-    return dominant_emotion
-
-
-
 def emotion_detector(text_to_analyse:str = '') -> str:
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = { 
@@ -32,6 +25,8 @@ def emotion_detector(text_to_analyse:str = '') -> str:
     
     if resp.status_code == 200:
         picked_response = resp.json().get('emotionPredictions', None)[0].get('emotion', None)
+        picked_response["dominant_emotion"] = max(picked_response, key = picked_response.get )
+        
         return picked_response
     else:
         print("Error:", resp.status_code, resp.text)
@@ -42,6 +37,5 @@ if __name__ =="__main__":
     
     test_str = "I love this new technology."
     emotions = emotion_detector(test_str)
-    print(f"{emotions=}")
-    dom = find_dominant_emotion(resp_emotions = emotions)
-    print(f"{dom=}")
+    pp(emotions)
+    
